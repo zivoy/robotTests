55e40d776291846c5777ee4704e526e4d4987992
zivoy <ziv132@gmail.com>
2019-03-14 05:48:49 +0000

    floats

--- robotFunctions.py
+++ robotFunctions.py
@@ -25,7 +25,7 @@ class mover:
     def __init__(self, m1, m2):
         self.m1 = ev3.LargeMotor(m1)
         self.m2 = ev3.LargeMotor(m2)
-    def drive(self, forward, turnDeg=0, turnDir=True, speed=200):
+    def drive(self, float(forward), turnDeg=0, turnDir=True, speed=200):
         wheel_circum = pi * wheel_diameter * multiplier
 
         robot_turn_circle = pi * robot_width
@@ -36,7 +36,7 @@ class mover:
 
         direction = 1.0 if not turnDir else -1.0
 
-        moveRot = forward / wheel_circum * 360
+        moveRot = forward / wheel_circum * 360.0
 
         if turnDir == 'right':
             rRot = rotation_deg + moveRot
