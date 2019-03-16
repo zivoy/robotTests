2d060bdc3f2240b377fddacb271059acc63fc3c9
vish-kik <avishalom@gmail.com>
2019-03-15 22:03:57 -0400

    put into function

--- navigate.py
+++ navigate.py
@@ -1,10 +1,10 @@
 import robotFunctions
-from ev3dev.ev3 import Sound
+from ev3dev.ev3 import Sound, Leds
 from time import sleep
 
 posOfCir = ['right', 'left', 'right', 'left']
 stageLength = 10
-
+green_area = 20
 
 def flip_place(val):
     if val == 'right':
@@ -17,28 +17,32 @@ robot = robotFunctions.RobotHandler('outA', 'outD', 'outB')
 
 robot.ar.position = 0
 
-process = robot.scan()
+def all_green():
+    process = robot.scan()
+    gCount = 0
+    for i, j in process:
+        if i == 'green':
+            i += 1
 
-gCount = 0
-for i, j in process:
-    if i == 'green':
-        i += 1
+    if gCount == len(process):
+        Leds.set_color(Leds.RIGHT, Leds.GREEN)
+        Leds.set_color(Leds.LEFT, Leds.GREEN)
+        Sound.speak("good to go, all green").wait()
+        Leds.all_off()
+        return True
 
-if gCount == len(process):
-    Sound.speak("good to go, all green").wait()
 
-
-# TODO handle first green
+# TODO: handle first green
 def go_rover():
-    drove = 0
+    drove = -green_area
     stage_count = 0
     while stage_count < len(posOfCir):
 
         input_type, input_feed = robot.scan_handler()
 
         if input_type == 'circle':
-            robot.turn_around_sensor()
-            to_edge = robot.circle_navigate(posOfCir[stage_count])  # TODO <--- do something with this
+            robot.turn_around_sensor(posOfCir[stage_count])
+            to_edge = robot.circle_navigate(posOfCir[stage_count])  # TODO: <--- do something with this
             drove = 0
             stage_count += 1
             continue
